a298631c3c05c7ebd0be7d2e85e2a5d467a5fb5e
Han <jervois@Jervoiss-MacBook-Pro.local>
2017-05-30 10:04:16 +0800

    controls
    controls

---
+++ game/controls.js
@@ -0,0 +1,62 @@
+var Ball = function(settings) {
+
+    // Settings
+    var ballElement = null;
+    var bullets = [];
+
+
+    function wall() {
+
+      var ballRect = ballElement.getBoundingClientRect();
+      var w = parseInt(window.innerWidth);
+      var h = parseInt(window.innerHeight);
+
+
+      if(ballRect.left < 0){
+          ballElement.style.left = '0px';
+      }
+
+      if(ballRect.right > w){
+          ballElement.style.left = ( w - ballRect.width) + 'px' ;
+      }
+
+
+
+    }
+
+    // Move the ball around manually
+    function move(interactions){
+
+      if(interactions.left){
+        ballElement.style.left = parseInt(ballElement.style.left)-14+"px";
+      }
+
+      if(interactions.right){
+        ballElement.style.left = parseInt(ballElement.style.left)+14+"px";
+      }
+
+      if(settings.walls){
+        wall();
+      }
+    }
+
+
+    function create() {
+        // Create the object asset
+    }
+
+    function init(){
+      // create();
+      ballElement = document.getElementById('ball');
+      ballElement.style.top = '400px'; //specifies the position of the ballElement
+      ballElement.style.left = '400px';
+      ballElement.style.height = '100px';
+
+    }
+
+    this.render = function(interactions){
+      move(interactions);
+    }
+
+    init();
+}
---
+++ game/fallingObjects.js
@@ -0,0 +1,12 @@
+// var fallingfurni = {};
+// var fallingpancakes = {};
+//
+//
+// function fallingObjects(){
+//
+// }
+// function resetRock(rock){
+//     rock.x=Math.random()*(canvas.width-rockWidth);
+//     rock.y=15+Math.random()*30;
+//     rock.speed=0.2+Math.random()*0.5;
+// }
